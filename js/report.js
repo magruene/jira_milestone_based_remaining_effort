a51760ffb7bdd21a2047cfcfc099e17c9a19ec71
magruene <marc_gruenewald@gmx.de>
2016-01-11 07:18:50 +0100

    Update report.js
    test

--- js/report.js
+++ js/report.js
@@ -43,7 +43,7 @@ function init() {
 function startReportGeneration() {
     resetTable();
 
-    var getAllEpicsForTeams = "http://jira.swisscom.com/rest/api/2/search?maxResults=500&jql=project=SAM and team in (Skipper, Catta, Yankee, Private, Rico, Kowalski) and (status != Closed or status != R4Review) and issueType = Epic and fixVersion in ('" + AJS.$("#versionChooserMain").val() + "', " + AJS.$("#versionChooserSmall").val() + "'";
+    var getAllEpicsForTeams = "http://jira.swisscom.com/rest/api/2/search?maxResults=500&jql=project=SAM and team in (Skipper, Catta, Yankee, Private, Rico, Kowalski) and (status != Closed or status != R4Review) and issueType = Epic and fixVersion in ('" + AJS.$("#versionChooserMain").val() + "', '" + AJS.$("#versionChooserSmall").val() + "'";
     ajaxCall(getAllEpicsForTeams, consolidateFutureEffort);
 
     AJS.$.each(teams, function (index, team) {
