1bd10560f0bb737a1b65c6fb1feb6b70a83f9f98
Marc Gruenewald (taagrma3) <marc.gruenewald1@swisscom.com>
2016-01-12 09:52:30 +0100

    try fix2

--- js/report.js
+++ js/report.js
@@ -1,4 +1,4 @@
-var teams = ["Skipper", "Catta", "Yankee", "Private", "Rico", "Kowalski"],
+var teams = ["Skipper"],
     possibleMilestoneLabels = ["R-20", "R-19", "R-18", "R-17", "R-16", "R-15", "R-14", "R-13", "R-12", "R-11", "R-10", "R-9", "R-8", "R-7", "R-6", "R-5", "R-4", "R-3", "R-2", "R-1", "R-0", "R1"],
     numberOfWeeksInThePast = 8,
     selectedMilestoneLabels,
